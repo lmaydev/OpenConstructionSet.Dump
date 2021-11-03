44ffac546158b9278520863cda2a59726b85cc87
Luke <allinighdrui@gmail.com>
2021-11-03 14:54:44 +0000

    Initial implementation

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-OpenConstructionSet.Dump
+# OpenConstructionSet.Dump
+
+A simple application that uses the [OpenConstructionSet](https://github.com/lmaydev/OpenConstructionSet) to load and dump your current [Kenshi](https://lofigames.com/) game data to JSON.
+
+## Usage
+
+By default `ocs-dump` will save to `data.json` in the active directory.
+If an argument is passed it will be treated as the output file.
+
+## Example
+
+Save to `data.json`
+
+`ocs-dump`
+
+Save to custom file
+
+`ocs-dump custom.json`
+
+`ocs-dump output/data.json`
+
+`ocs-dump C:/output/data.json`
---
+++ src/OpenConstructionSet.Dump/Program.cs
@@ -0,0 +1,101 @@
+﻿using OpenConstructionSet;
+using OpenConstructionSet.Data;
+using OpenConstructionSet.Models;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+var file = args.Length > 0 ? args[0] : "data.json";
+
+var installations = OcsDiscoveryService.Default.DiscoverAllInstallations();
+
+Installation? installation = installations.Count switch
+{
+    0 => null,
+    1 => installations.Values.First(),
+    _ => PromptInstallationChoice()
+};
+
+if (installation is null)
+{
+    Console.WriteLine("Failed to find game");
+    return 1;
+}
+
+Console.WriteLine();
+
+Console.Write("Building data...");
+
+var options = new OcsDataContexOptions(Name: Guid.NewGuid().ToString(),
+                                       Installation: installation,
+                                       LoadGameFiles: ModLoadType.Base,
+                                       LoadEnabledMods: ModLoadType.Base,
+                                       ThrowIfMissing: false);
+
+var items = OcsDataContextBuilder.Default.Build(options).Items.Values.ToList();
+
+Console.WriteLine(" Complete");
+
+var jsonOptions = new JsonSerializerOptions
+{
+    WriteIndented = true,
+    Converters = { new JsonStringEnumConverter() }
+};
+
+try
+{
+    Console.Write("Serializing to file...");
+
+    var directory = Path.GetDirectoryName(file);
+
+    if (!string.IsNullOrWhiteSpace(directory) && !Directory.Exists(directory))
+    {
+        Directory.CreateDirectory(directory);
+    }
+
+    File.Delete(file);
+
+    using var stream = File.Create(file);
+
+    JsonSerializer.Serialize(stream, items, jsonOptions);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(" Failed");
+
+    Console.WriteLine();
+    Console.WriteLine(ex.ToString());
+    return 1;
+}
+
+Console.WriteLine(" Complete");
+
+return 0;
+
+Installation PromptInstallationChoice()
+{
+    var keys = installations.Keys.ToList();
+
+    Console.WriteLine("Multiple installations found");
+
+    // Output names of found installations
+    for (var i = 0; i < keys.Count; i++)
+    {
+        Console.WriteLine($"{i + 1} - {keys[i]}");
+    }
+
+    Console.Write("Please select which to use: ");
+
+    // Read the users input defaulting to the first installation if invalid
+    if (!int.TryParse(Console.ReadLine() ?? "1", out var choice) || choice < 1 || choice > installations.Count)
+    {
+        choice = 1;
+    }
+
+    var selectedKey = keys[choice - 1];
+
+    Console.WriteLine();
+
+    Console.WriteLine($"Using the {selectedKey} installation");
+
+    return installations[selectedKey];
+}
